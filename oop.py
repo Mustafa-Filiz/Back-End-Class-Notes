ef4702a1dad6e370357beaa37e4a5e00e634b455
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-10-06 22:15:35 +0300

    lesson 3

--- oop.py
+++ oop.py
@@ -86,12 +86,100 @@ os.system("cls" if os.name == "nt" else "clear")
 
 # Special methods
 
+# class Person:
+# 	company = "Clarusway"
+
+# 	def __init__(self, name, age):
+# 		self.name = name
+# 		self.age = age
+
+# 	def __str__(self):
+# 		return f"Name : {self.name}   Age: {self.age}"
+
+# 	def __len__(self):
+# 		return self.age
+
+# person1 = Person("Barry", 44)
+# print(person1.name, person1.age)
+
+# lst = [1,2,3]
+# print(len(lst))
+# print(len(person1))
+
+# abstraction and encapsulation
+
+# lst = [3,2,5,9,1]
+# lst.sort()
+# print(lst)
+
+# class Person:
+#     company = "Clarusway"
+
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#         self._id = 5000
+#         self.__id1 = 4000
+
+#     def __str__(self):
+#         return f"Name: {self.name}  Age: {self.age}"
+
+# person1 = Person("Rafe", 39)
+# person1._id = 3000
+# print(person1._id)
+# print(person1.__id1)
+
+# class Person:
+#     company = "Clarusway"
+
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+#     def __str__(self):
+#         return f"Name: {self.name}  Age: {self.age}"
+
+
+# class Employee(Person):
+#     def __init__(self, name, age, path):
+#         self.name = name
+#         self.age = age
+#         self.path = path
+
+
+# emp1 = Employee("Barry", 44, "FS")
+# print(emp1)
+
+
+# inheritance and polymorphise
+
 class Person:
-	company = "Clarusway"
+    company = "Clarusway"
 
-	def __init__(self, name, age):
-		self.name = name
-		self.age = age
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
-person1 = Person("Barry", 44)
-print(person1.name, person1.age)
+    def __str__(self):
+        return f"Name: {self.name}  Age: {self.age}"
+
+class Lang:
+	def __init__(self, langs):
+		self.langs = langs
+
+
+class Employee(Person, Lang):
+    def __init__(self, name, age, path):
+        # self.name = name
+        # self.age = age
+        super().__init__(name, age)
+        self.path = path
+
+    def __str__(self):
+        return f"Name: {self.name}  Age: {self.age}   Path: {self.path}"
+
+
+emp1 = Employee("Barry", 44, "FS")
+print(emp1)
+
+print(Employee.mro())
