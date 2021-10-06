8e7bf54213007f93b7d5406d22f2c0a30f226e2f
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-10-06 21:05:14 +0300

    first lesson

--- oop.py
+++ oop.py
@@ -1,32 +1,97 @@
-import os 
+import os
 os.system("cls" if os.name == "nt" else "clear")
 
-def printtype(data):
-    for i in data:
-        print(i, type(i))
+# def printtype(data):
+#     for i in data:
+#         print(i, type(i))
 
 
-test =[123, "Barry", [1,2,3], (1,2,3), {1,2,3}, True, lambda x: x,{
-    "name": "henry", "age": 38
-}]
+# test =[123, "Barry", [1,2,3], (1,2,3), {1,2,3}, True, lambda x: x,{
+#     "name": "henry", "age": 38
+# }]
 
 # printtype(test)
 
 # Defining Class
 
-class Person:
-    name = "Barry"
-    age = 44
+# class Person:
+#     name = "Barry"
+#     age = 44
 
-person1 = Person()
-person2 = Person()
+# person1 = Person()
+# person2 = Person()
 
-print(person1.name)
-print(person2.name)
+# print(person1.name)
+# print(person2.name)
 
-Person.job = "teacher"
-print(person1.job)
-print(person2.job)
+# Person.job = "teacher"
+# print(person1.job)
+# print(person2.job)
 
 
 #  Class attributes ve instance attributes
+
+# Person.name = "Rafael"
+# person1.name = "Henry"
+# print(person1.name)
+# print(person2.name)
+
+# SELF keyword
+
+# class Person:
+#     name = "Barry"
+#     age = 44
+#     def test(self):
+# 	    print("test")
+
+#     def get_details(self):
+# 	    print("name: ", self.name, "age: ", self.age , "location: ", self.location)
+
+#     def set_details(self, name, age, location):
+#         self.name = name
+#         self.age = age
+#         self.location = location
+
+# person1 = Person()
+# person1.test()
+# Person.test(person1)
+# person1.set_details("Henry", 38, "Ankara")
+# person1.get_details()
+
+
+# class Person:
+#     name = "Barry"
+#     age = 44
+
+#     def test(self):
+#         print("test")
+
+#     def get_details(self):
+#         print("name: ", self.name, "age: ",
+#               self.age, "location: ", self.location)
+
+#     def set_details(self, name, age, location):
+#         self.name = name
+#         self.age = age
+#         self.location = location
+
+#     @staticmethod
+#     def salute():
+#         print("Hi there! ", Person.name)
+
+# Person.salute()
+# person1 = Person()
+# person1.salute()
+
+
+# Special methods
+
+class Person:
+	company = "Clarusway"
+
+	def __init__(self, name, age):
+		self.name = name
+		self.age = age
+
+person1 = Person("Barry", 44)
+print(person1.name, person1.age)
