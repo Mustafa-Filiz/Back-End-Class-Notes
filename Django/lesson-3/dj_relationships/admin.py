1e398c487e7e2cb5194dd1bb7b43c212234c6d60
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-02 19:49:54 +0300

    tables added

--- Django/lesson-3/dj_relationships/admin.py
+++ Django/lesson-3/dj_relationships/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from .models import Creator, Language
+from .models import Creator, Language, Framework, Programmer
 
 
 # Register your models here.
 
 admin.site.register(Creator)
 admin.site.register(Language)
+admin.site.register(Framework)
+admin.site.register(Programmer)
+
 
