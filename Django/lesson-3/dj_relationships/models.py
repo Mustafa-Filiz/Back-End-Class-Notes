32ce2f53580cf8391d67cc4cd3e8f61312db09eb
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-02 19:50:16 +0300

    tables added

--- Django/lesson-3/dj_relationships/models.py
+++ Django/lesson-3/dj_relationships/models.py
@@ -16,3 +16,17 @@ class Language(models.Model):
     def __str__(self):
         return self.name
 	
+class Framework(models.Model):
+    name = models.CharField(max_length=30)
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+	
+class Programmer(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    framework = models.ManyToManyField(Framework)
+    
+    def __str__(self):
+        return self.first_name
