18594d9ded8b0b95b05d8bbc08ad59dab0cc49cb
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-03 15:05:02 +0300

    home template added

--- Django/lesson-4/app/views.py
+++ Django/lesson-4/app/views.py
@@ -1,7 +1,12 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
+# from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello World!")
+    context = {
+        'title' : "Clarusway",
+        'dict_1' : { 'Django' : 'Framework'},
+        'my_list' : [2, 3, 4]
+    }
+    return render(request, 'app/home.html', context)
