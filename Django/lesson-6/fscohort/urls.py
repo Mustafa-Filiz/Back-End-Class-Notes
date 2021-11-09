acbc20f14a9621ba9402782102e18e0bbdf9b7b7
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-09 22:32:44 +0300

    student add url added

--- Django/lesson-6/fscohort/urls.py
+++ Django/lesson-6/fscohort/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home, student_list
+from .views import home, student_list, student_add
 
 urlpatterns = [
     path('', home, name="home"),
     path('student_list/', student_list, name="list"),
+    path('student_add/', student_add, name="list"),
 ]
