5615bee8d43b7be8225e7eb2eebd9ffcd6b7e644
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-09 16:50:32 +0300

    student model created

---
+++ Django/lesson-6/fscohort/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    phone = models.CharField(max_length=50)
+    number = models.CharField(max_length=50)
+    GENDER = (
+		("1", "Male"),
+		("2", "Female"),
+		("3", "Other"),
+		("4", "Prefer not to say"),
+	)
+    gender = models.CharField(max_length=50, choices=GENDER)
+    image = models.ImageField(upload_to="student/", default="default.png")
+    
+    
