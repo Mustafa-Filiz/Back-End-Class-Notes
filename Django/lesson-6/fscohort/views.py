83573dee4e52c8ffc905c6594d1e5f0c6150d798
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-09 21:07:52 +0300

    updates

--- Django/lesson-6/fscohort/views.py
+++ Django/lesson-6/fscohort/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse(request, '<h1>Welcome Django!</h1>')
+    return HttpResponse('<h1>Welcome Django!</h1>')
