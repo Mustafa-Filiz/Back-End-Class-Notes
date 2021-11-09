25d08be95963badfc2c378d7464a3127d2125199
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-09 22:31:48 +0300

    crispy fprms added

--- Django/lesson-6/main/settings.py
+++ Django/lesson-6/main/settings.py
@@ -41,7 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     #apps
-    'fscohort'
+    'fscohort',
+    
+    #third party
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -132,3 +135,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
