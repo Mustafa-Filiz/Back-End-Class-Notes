d96250ffe3c863d3c0e68c949700b0713087c37d
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-09 22:11:46 +0300

    list url added

--- Django/lesson-6/fscohort/urls.py
+++ Django/lesson-6/fscohort/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, student_list
 
 urlpatterns = [
-    path('', home),
+    path('', home, name="home"),
+    path('student_list/', student_list, name="list"),
 ]
